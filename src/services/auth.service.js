b98ac025ab0b4bc3105a81c4f872dfd37e1b3bdd
kaano29 <kaan.coskun7@gmail.com>
2022-03-25 12:11:58 +0100

    url changed for deployment

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const API_URL = "http://localhost:8080/";
+const API_URL = "https://charging-stations-backend.azurewebsites.net/";
 
 const register = (username, email, password) => {
   return axios.post(API_URL + "register", {
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://localhost:8080/api/test/";
+const API_URL = "https://charging-stations-backend.azurewebsites.net/api/test";
 
 const getPublicContent = () => {
   return axios.get(API_URL + "all");
