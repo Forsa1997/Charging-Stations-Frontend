ebb7b47fdca60c55108dd7d0fa3cb06d0ade6fc6
kaano29 <kaan.coskun7@gmail.com>
2022-03-29 13:44:50 +0200

    retry browserrouter

--- src/App.js
+++ src/App.js
@@ -18,13 +18,12 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/map" element={<Map />} />
-        <Route path="/register" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/logout" element={<Login />} />
+        <Route path={process.env.PUBLIC_URL + "/map"} element={<Map />} />
+        <Route path={process.env.PUBLIC_URL + "/register"} element={<SignUp />} />
+        <Route path={process.env.PUBLIC_URL + "/login"} element={<Login />} />
+        <Route path={process.env.PUBLIC_URL + "/profile"} element={<Profile />} />
+        <Route path={process.env.PUBLIC_URL + "/logout"} element={<Login />} />
       </Routes>
-      <script src="/build/client.entry.js" />
     </Provider>
   );
 }
