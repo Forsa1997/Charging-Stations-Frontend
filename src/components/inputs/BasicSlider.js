36c8164e93bc12e1b2ffe2dce39492f1a77d89d4
Christoph Ruhe <Christoph.Ruhe@web.de>
2022-03-30 13:54:55 +0200

    Added the all filters + did some styling

--- src/components/inputs/BasicSlider.js
+++ src/components/inputs/BasicSlider.js
@@ -4,41 +4,41 @@ import Slider from '@mui/material/Slider';
 import Typography from '@mui/material/Typography';
 
 const marks = [
-  {
-    value: 0,
-    label: '0KW',
-  },
-  {
-    value: 50,
-    label: '50KW',
-  },
-  {
-    value: 150,
-    label: '150KW',
-  },
-  {
-    value: 300,
-    label: '300KW',
-  },
+    {
+        value: 0,
+        label: '0KW',
+    },
+    {
+        value: 50,
+        label: '50KW',
+    },
+    {
+        value: 150,
+        label: '150KW',
+    },
+    {
+        value: 300,
+        label: '300KW',
+    },
 ];
 
 function valuetext(value) {
-  return `${value}KW`;
+    return `${value}KW`;
 }
 
 export default function DiscreteSliderLabel() {
-  return (
-    <Box sx={{ width: 300 }}>
-               <Typography gutterBottom>Charging Power</Typography>
-      <Slider
-        aria-label="Always visible"
-        defaultValue={50}
-        getAriaValueText={valuetext}
-        step={5}
-        marks={marks}
-        valueLabelDisplay="on"
-        max={300}
-      />
-    </Box>
-  );
+    return (
+        <Box sx={{ width: 300, pt: 3 }}>
+            <Typography gutterBottom>Charging Power</Typography>
+            <Slider
+                aria-label="Always visible"
+                defaultValue={50}
+                getAriaValueText={valuetext}
+                step={5}
+                marks={marks}
+                valueLabelDisplay="off"
+                max={300}
+            />
+        </Box>
+    );
 }
