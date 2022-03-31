a530aeb734fee0332589c18dba7e7bc7701899ee
Christoph Ruhe <Christoph.Ruhe@web.de>
2022-03-31 11:55:04 +0200

    Changed the popups and fixed the warnings

--- src/components/ChargingMap.js
+++ src/components/ChargingMap.js
@@ -6,29 +6,28 @@ import MarkerClusterGroup from 'react-leaflet-markercluster';
 
 export default function ChargingMap() {
 
-// const filteredStations = teslaData.filter(tesla => tesla.address.country ==="Germany")
+    // const filteredStations = teslaData.filter(tesla => tesla.address.country ==="Germany")
 
-    return(
-     <MapContainer center={[52.3758916, 9.7320104]} zoom={6} s>
-     <TileLayer
-         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-     />
-     <MarkerClusterGroup>
-         {stationData.map(station => (
-             <Marker key={station.id}
-                 position={[station.addressInfo.latitude, station.addressInfo.longitude]} >
-                 {/* <Popup>
-                     <h2>Status: {tesla.status}<br /></h2>
-                     {tesla.name}<br />
-                     {tesla.address.street}, {tesla.address.city}<br />
-                     Stallcount: {tesla.stallCount}<br />
-                     Max Charge Power: {tesla.powerKilowatt} KW
-                 </Popup> */}
-             </Marker>
-         ))}
-     </MarkerClusterGroup>
- </MapContainer>
+    return (
+        <MapContainer center={[52.3758916, 9.7320104]} zoom={6} s>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <MarkerClusterGroup>
+                {stationData.map(station => (
+                    <Marker key={station.id}
+                        position={[station.addressInfo.latitude, station.addressInfo.longitude]} >
+                        <Popup>
+                            <h2>{station.connections.filter(connection => connection.statusTypeID === 50).length} out of {station.connections.length} stations are working!</h2>
+                            <h4>{station.addressInfo.addressLine1}, {station.addressInfo.town}</h4>
+                            <h4>Max Charge Power: {Math.max.apply(Math, station.connections.map(function (o) { return o.powerKW }))} KW</h4>
 
-)
+                        </Popup>
+                    </Marker>
+                ))}
+            </MarkerClusterGroup>
+        </MapContainer>
+
+    )
 }
