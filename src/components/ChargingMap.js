25debee51322279243896a32c4a154eb56b56045
Christoph Ruhe <Christoph.Ruhe@web.de>
2022-03-31 18:04:20 +0200

    strted with the filters

--- src/components/ChargingMap.js
+++ src/components/ChargingMap.js
@@ -2,21 +2,20 @@
 import { TileLayer,  Popup } from 'react-leaflet'
 // eslint-disable-next-line
 import MarkerClusterGroup from 'react-leaflet-markercluster';
-import stationData from "../data/stationData.json"
 import L from 'leaflet';
 import { useEffect } from 'react';
-
+import { useSelector } from 'react-redux';
 
 export default function ChargingMap() {
 
+    const data = useSelector (state => state.mapReducer.filteredData);
+
     useEffect(() => {
+    
         var map = L.map('map').setView([52.3758916, 9.7320104], 8)
-
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-
         var markers = L.markerClusterGroup();
-        
-        stationData.map(station => {
+        data.map(station => {
             const marker = L.marker([
                 station.addressInfo.latitude,
                 station.addressInfo.longitude
---
+++ src/reducers/mapReducer.js
@@ -0,0 +1,38 @@
+import {
+    FILTER_CHARGINGPOWER,
+    FILTER_PLUGTYPE,
+    FILTER_OPERATOR,
+    FILTER_FREETOUSE,
+} from "../actions/types";
+import stationData from "../data/stationData.json"
+
+const initialState = {data: stationData, filteredData: stationData};
+
+const mapReducer = (state = initialState, action) => {
+    const { type, payload } = action;
+    switch (type) {
+        case FILTER_CHARGINGPOWER:
+            // Math.max.apply(Math, stations.connections.map(function(o) { return o.powerKW; }))
+            return {...state, filteredData: state.data.filter(station => station.connections.filter(connection => connection.powerKW >= payload.kw).length >= 1)}
+
+        case FILTER_PLUGTYPE:
+            return {
+                ...state,
+                isLoggedIn: false,
+            };
+        case FILTER_OPERATOR:
+            return {
+                ...state,
+                isLoggedIn: false,
+            };
+        case FILTER_FREETOUSE:
+            return {
+                ...state,
+                isLoggedIn: false,
+            };
+        default:
+            return state;
+    }
+}
+
+export default mapReducer;
