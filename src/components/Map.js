5ecf862e629ae789eafd1e36f6d3a524cf1db3a6
Christoph Ruhe <Christoph.Ruhe@web.de>
2022-04-01 18:15:52 +0200

    fixed warnings and merge conflicts

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,26 +1,38 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import ChargingMap from './ChargingMap';
-<<<<<<< HEAD
 import PrimarySearchAppBar from "./inputs/PrimarySearchAppBar";
 import Collapse from "@mui/material/Collapse";
 import Card from "@mui/material/Card";
-import {StyledEngineProvider} from "@mui/material/styles";
+import { StyledEngineProvider } from "@mui/material/styles";
 import BasicTextField from "./inputs/BasisTextfield";
 import BasicSlider from "./inputs/BasicSlider";
 import BasicSelect from "./inputs/BasicSelect";
 import ChipSelect from "./inputs/ChipSelect";
 import referenceData from "../data/referenceData.json";
 import CloseIcon from '@mui/icons-material/Close';
-import {IconButton} from "@mui/material";
+import { IconButton } from "@mui/material";
+import { GET_NEW_DATA } from '../actions/types';
+import stationData from "../data/stationData.json"
+import { useDispatch } from 'react-redux';
+
+
+
 
 
 const Map = () => {
+
+    const dispatch = useDispatch();
+    dispatch({
+        type: GET_NEW_DATA,
+        payload: stationData,
+    })
+
     const [checked, setChecked] = React.useState(false);
 
-    const plugTypes = [{ value: "type2", name: "Type 2" }, { value: "ccs", name: "CCS" }, { value: "all", name: "All" }]
+    const plugTypes = [{ value: "all", name: "All" }, { value: "type2", name: "Type 2" }, { value: "ccs", name: "CCS" }]
     const chargingProviders = referenceData.Operators
-    const chargingFree = [{ value: "no", name: "No" }, { value: "yes", name: "Yes" }]
+    const chargingFree = [{ value: "all", name: "All" }, { value: "no", name: "No" }, { value: "yes", name: "Yes" }]
     const marks = [
         {
             value: 0,
@@ -63,45 +75,29 @@ const Map = () => {
         }}
         >
             <Box>
-            <IconButton sx={{float: 'right'}} onClick={handleOnMenuClick}>
-                <CloseIcon color='grey' fontSize='large'/>
-            </IconButton>
+                <IconButton sx={{ float: 'right' }} onClick={handleOnMenuClick}>
+                    <CloseIcon color='grey' fontSize='large' />
+                </IconButton>
             </Box>
             <BasicTextField />
             <BasicSlider marks={marks} max={300} min={0} steps={5} default={50} />
             <BasicSelect filterType="FILTER_PLUGTYPE" values={plugTypes} header="Plug Type" />
-            <ChipSelect  values={chargingProviders} header="Operator" />
+            <ChipSelect values={chargingProviders} header="Operator" />
             <BasicSelect filterType="FILTER_FREETOUSE" values={chargingFree} header="Free to use" />
         </Card>
 
-)
+    )
 
-
-=======
-import BasicPopover from './inputs/BasicPopover';
-import { GET_NEW_DATA } from '../actions/types';
-import stationData from "../data/stationData.json"
-import { useDispatch } from 'react-redux';
-
-
-
-const Map = () => {
-    const dispatch = useDispatch();
-    dispatch({
-        type: GET_NEW_DATA,
-        payload: stationData,
-    })
->>>>>>> mapTestScale
 
     return (
         <StyledEngineProvider injectFirst>
 
-            <Box sx={{width: '100vw', height: 'calc(100vh - 68px)', display: 'flex'}}>
+            <Box sx={{ width: '100vw', height: 'calc(100vh - 68px)', display: 'flex' }}>
                 <Collapse orientation="horizontal" in={checked}>
                     {card}
                 </Collapse>
-                <PrimarySearchAppBar handleOnMenuClick={handleOnMenuClick}/>
-                <ChargingMap/>
+                <PrimarySearchAppBar handleOnMenuClick={handleOnMenuClick} />
+                <ChargingMap />
             </Box>
         </StyledEngineProvider>
 
